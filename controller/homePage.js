657e05578df8e3486a2721756d7bc8d32befd55a
Cécile <peigne.cecile@free.fr>
2019-05-08 22:04:09 +0200

    display ckeditor

--- controller/homePage.js
+++ controller/homePage.js
@@ -3,5 +3,20 @@ const Post = require('../database/models/Article'),
 
 module.exports = async (req, res) => {
     const posts = await Post.find({});
+    console.log(posts);
+
+    decodeHtml = function(str)
+    {
+        const map =
+        {
+            '&amp;': '&',
+            '&lt;': '<',
+            '&gt;': '>',
+            '&quot;': '"',
+            '&#039;': "'"
+        };
+        return str.replace(/&amp;|&lt;|&gt;|&quot;|&#039;/g, function(m) {return map[m];});
+    }
+    
         res.render('index', { posts });
 }
